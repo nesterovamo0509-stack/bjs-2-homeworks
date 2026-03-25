6959bf0d929766855e0a3891916766003fef0c87
Margarita <nesterovamo0509@gmail.com>
2026-03-25 17:28:53 +0300

    Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filtered = users.filter(user => user.gender === gender);
+
+    if (filtered.length === 0) {
+        return 0;
+    }
+
+    return users.filter(user => user.gender === gender).map(user => user.age).reduce((sum, age, _, arr) => sum + age / arr.length, 0);
 }
